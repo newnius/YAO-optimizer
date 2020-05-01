5678dcf770c6d9017a4c161642bd7cad44e2d872
Newnius <newnius.cn@gmail.com>
2020-05-01 19:45:26 +0800

    update

--- serve.py
+++ serve.py
@@ -216,7 +216,7 @@ class MyHandler(BaseHTTPRequestHandler):
 				with open(config.train_data_path, 'r') as f:
 					df = pd.read_csv(config.train_data_path,
 					                 usecols=['job', 'model', 'time', 'utilCPU', 'utilGPU', 'pre', 'main', 'post'])
-					df = df.tail(config.time_step)
+					df = df.tail(config.time_step - 1)
 					df = df.append(data, ignore_index=True)
 					df.to_csv('./data/test_data.csv', index=False)
 
