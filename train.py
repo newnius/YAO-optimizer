d9e6e7ab26af34448eacf92172c2799ba383a996
Newnius <newnius.cn@gmail.com>
2020-05-02 12:34:27 +0800

    update

--- train.py
+++ train.py
@@ -104,7 +104,7 @@ def experiment(repeats, series, seed):
 		t2 = test.shape[0] % batch_size
 
 		train_trimmed = train_scaled[train_scaled.shape[0] - t1 * batch_size:, :]
-		lstm_model = fit_lstm(train_trimmed, batch_size, 3000, 4)
+		lstm_model = fit_lstm(train_trimmed, batch_size, 300, 4)
 		# forecast the entire training dataset to build up state for forecasting
 		print(train_trimmed)
 		print(train_trimmed[:, 0])
